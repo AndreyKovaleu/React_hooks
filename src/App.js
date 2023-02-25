ff63232182a739b085a2333bd14f508087f453db
AndreyKovaleu <Andreykovaleu@gmail.com>
2023-02-25 20:08:39 +0300

    lesson 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import ClassCounter from "./components/ClassCounter";
+import HookCounter from "./components/HookCounter";
+
 function App() {
   return (
     <div className="App">
+      {/* <ClassCounter /> */}
+      <HookCounter />
     </div>
   );
 }
