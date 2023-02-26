3894136f9f4872472945a7e8ee8a7ea041aa48de
AndreyKovaleu <Andreykovaleu@gmail.com>
2023-02-26 19:42:16 +0300

    lesson 16

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import React from "react";
+import ComponentC from "./components/ComponentC";
+
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
+
 function App() {
   return (
     <div className="App">
+      <UserContext.Provider value={'Andrey'}>
+        <ChannelContext.Provider value={"Cod111"}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
