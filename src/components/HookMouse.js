e1852e11061599261d43e9dc5e86d3fe43eb3d17
AndreyKovaleu <Andreykovaleu@gmail.com>
2023-02-26 16:29:01 +0300

    lesson 9

---
+++ src/components/HookMouse.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+
+function HookMouse() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const logMousePosition = e => {
+    console.log("logMousePosition");
+    setX(e.clientX);
+    setY(e.clientY);
+  }
+
+  useEffect(() => {
+    console.log("useEffect");
+    window.addEventListener("mousemove", logMousePosition)
+  }, [])
+
+  return (
+    <div>
+      X - {x}
+      <br />
+      Y - {y}
+    </div>
+  )
+}
+
+export default HookMouse
