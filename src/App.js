0b0881d348326b24f6e678691ad36eba11f652fc
AndreyKovaleu <Andreykovaleu@gmail.com>
2023-02-27 19:53:01 +0300

    lesson 22

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
+import React, { useReducer } from "react";
+import ComponentA from "./components/ComponentA";
+import ComponentB from "./components/ComponentB";
+import ComponentC from "./components/ComponentC";
 import CounterOne from "./components/CounterOne";
 import CounterThree from "./components/CounterThree";
 import CounterTwo from "./components/CounterTwo";
 
+export const CountContext = React.createContext()
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return initialState;
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
-      {/* <CounterOne /> */}
-      {/* <CounterTwo /> */}
-      <CounterThree />
-    </div>
+    <CountContext.Provider value={{ countState: count, countDispatch: dispatch }}>
+      <div className="App">
+        {/* <CounterOne /> */}
+        {/* <CounterTwo /> */}
+        {/* <CounterThree /> */}
+        Count - {count}
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </div>
+    </CountContext.Provider>
   );
 }
 
