5784f74d3498d6c4ca4936e3998c9a74c19d48b5
AndreyKovaleu <Andreykovaleu@gmail.com>
2023-03-02 00:30:20 +0300

    lesson 32

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import CounterOne from "./components/CounterOne";
+import CounterTwo from "./components/CounterTwo";
 import DocTitleOne from "./components/DocTitleOne";
 import DocTitleTwo from "./components/DocTitleTwo";
 
 function App() {
   return (
     <div className="App">
-      <DocTitleOne />
-      <DocTitleTwo />
+      {/* <DocTitleOne />
+      <DocTitleTwo /> */}
+      <CounterOne />
+      <CounterTwo />
     </div>
   );
 }
